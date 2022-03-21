2539dda06e5656871778308cc720ef4fd5721b33
Basharabulaban <ba.ahmad@gmail.com>
2022-03-21 12:07:11 +0300

    lanch the app

--- spring/spring-db-access/src/main/java/com/example/service/Fruit.java
+++ spring/spring-db-access/src/main/java/com/example/service/Fruit.java
@@ -1,22 +1,39 @@
 package com.example.service;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Fruit {
 
-    public Fruit(String name) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Fruit() {
+    }
+
+    public Fruit(String type) {
+        this.name = type;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public void setId(Long id) {
+        this.id = id;
+    }
 
-    }
-    public Long getId() {
-        return 0L;
-    }
     public String getName() {
-        return null;
+        return name;
     }
 
     public void setName(String name) {
-
+        this.name = name;
     }
-
 }
