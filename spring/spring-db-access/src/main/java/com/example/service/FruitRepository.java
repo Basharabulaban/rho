326f25373d041d1c1825b84914179b7614fcfac2
Basharabulaban <ba.ahmad@gmail.com>
2022-03-21 18:15:38 +0300

    to start the App

--- spring/spring-db-access/src/main/java/com/example/service/FruitRepository.java
+++ spring/spring-db-access/src/main/java/com/example/service/FruitRepository.java
@@ -1,26 +1,48 @@
+//package com.example.service;
+//
+//import org.springframework.stereotype.Component;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Component
+//public class FruitRepository {
+//    public List<Fruit> findAll() {
+//        return null;
+//    }
+//
+//    public Fruit save(Fruit fruit) {
+//
+//        return fruit;
+//    }
+//
+//    public Optional<Fruit> findById(Long id) {
+//        return null;
+//    }
+//
+//    public void deleteById(Long id) {
+//
+//    }
+//}
+
+
 package com.example.service;
 
-import org.springframework.stereotype.Component;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
-@Component
-public class FruitRepository {
-    public List<Fruit> findAll() {
-        return null;
-    }
+@Repository
+public interface FruitRepository extends CrudRepository<Fruit, Long> {
+	  List<Fruit> findByName(String name);
 
-    public Fruit save(Fruit fruit) {
+	    default List<Fruit> findAllFruitsByName(String name) {
+	        return findByName(name);
+	    }
 
-        return fruit;
-    }
-
-    public Optional<Fruit> findById(Long id) {
-        return null;
-    }
-
-    public void deleteById(Long id) {
-
-    }
+	    @Query("select f from Fruit f where f.name like %?1")
+	    List<Fruit> findByNameLike(String name);
+// TODO query methods
 }
