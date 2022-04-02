150ba67d107ed5d3e2c4ee21fbcaf6c752594d33
Basharabulaban <ba.ahmad@gmail.com>
2022-04-02 23:22:49 +0300

    fruit

--- spring/spring-db-access/src/main/java/com/example/service/Fruit.java
+++ spring/spring-db-access/src/main/java/com/example/service/Fruit.java
@@ -1,15 +1,21 @@
 package com.example.service;
 
+//import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+//import javax.persistence.Table;
+
+//import com.sun.istack.NotNull;
 
 @Entity
+
 public class Fruit {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
 
     private String name;
